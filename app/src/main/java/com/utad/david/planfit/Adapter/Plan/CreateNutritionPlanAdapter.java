ae53e0f4f18da63ac9c20b73b0cf59803e9d36f3
David <davidgimenez1997clase@gmail.com>
2019-03-23 13:03:55 +0100

    add plan sport and nutrition

--- app/src/main/java/com/utad/david/planfit/Adapter/Plan/CreateNutritionPlanAdapter.java
+++ app/src/main/java/com/utad/david/planfit/Adapter/Plan/CreateNutritionPlanAdapter.java
@@ -1,4 +1,78 @@
 package com.utad.david.planfit.Adapter.Plan;
 
-public class CreateNutritionPlanAdapter {
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.utad.david.planfit.Model.Nutrition.DefaultNutrition;
+import com.utad.david.planfit.Model.Sport.DefaultSport;
+import com.utad.david.planfit.R;
+
+import java.util.List;
+
+public class CreateNutritionPlanAdapter extends RecyclerView.Adapter<CreateNutritionPlanAdapter.CreatePlanNutritionViewHolder> {
+
+    private List<DefaultNutrition> defaultNutritions;
+
+    private CreateNutritionPlanAdapter.OnItemClickListener listener;
+
+    public interface OnItemClickListener {
+        void onItemClick(DefaultNutrition item);
+    }
+
+
+    public CreateNutritionPlanAdapter(List<DefaultNutrition> defaultNutritions, CreateNutritionPlanAdapter.OnItemClickListener listener) {
+        this.defaultNutritions = defaultNutritions;
+        this.listener = listener;
+    }
+
+    @Override
+    public CreateNutritionPlanAdapter.CreatePlanNutritionViewHolder onCreateViewHolder(ViewGroup parent,
+                                                                               int viewType) {
+        View rootView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_recycleview, parent, false);
+        return new CreateNutritionPlanAdapter.CreatePlanNutritionViewHolder(rootView);
+    }
+
+    @Override
+    public void onBindViewHolder(CreateNutritionPlanAdapter.CreatePlanNutritionViewHolder holder, int position) {
+        final DefaultNutrition current = defaultNutritions.get(position);
+        holder.setData(current);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(listener!=null){
+                    listener.onItemClick(current);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return defaultNutritions.size();
+    }
+
+    public static class CreatePlanNutritionViewHolder extends RecyclerView.ViewHolder {
+        public TextView nameSlimming;
+        private ImageView photoSlimming;
+
+        public CreatePlanNutritionViewHolder(View v) {
+            super(v);
+            nameSlimming = v.findViewById(R.id.nameSlimming);
+            photoSlimming = v.findViewById(R.id.imageSlimming);
+        }
+
+        public void setData(DefaultNutrition defaultNutrition){
+            nameSlimming.setText(defaultNutrition.getName());
+            RequestOptions requestOptions = new RequestOptions();
+            requestOptions.placeholder(R.drawable.icon_gallery);
+            Glide.with(itemView).setDefaultRequestOptions(requestOptions).load(defaultNutrition.getPhoto()).into(photoSlimming);
+        }
+    }
 }
+
+
--- app/src/main/java/com/utad/david/planfit/Fragments/FragmentsMainMenuActivity/CreatePlan/Nutrition/NutritionCreatePlanFragment.java
+++ app/src/main/java/com/utad/david/planfit/Fragments/FragmentsMainMenuActivity/CreatePlan/Nutrition/NutritionCreatePlanFragment.java
@@ -1,32 +1,180 @@
 package com.utad.david.planfit.Fragments.FragmentsMainMenuActivity.CreatePlan.Nutrition;
 
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import butterknife.ButterKnife;
+import com.utad.david.planfit.Adapter.Favorite.NutritionFavoriteAdapter;
+import com.utad.david.planfit.Adapter.Plan.CreateNutritionPlanAdapter;
+import com.utad.david.planfit.Data.Firebase.FirebaseAdmin;
+import com.utad.david.planfit.Data.SessionUser;
+import com.utad.david.planfit.DialogFragment.Favorite.NutritionFavoriteDetailsDialogFragment;
+import com.utad.david.planfit.DialogFragment.Plan.CreateNutritionPlanDetailsDialogFragment;
+import com.utad.david.planfit.Model.Nutrition.DefaultNutrition;
+import com.utad.david.planfit.Model.Sport.DefaultSport;
 import com.utad.david.planfit.R;
 
-public class NutritionCreatePlanFragment extends Fragment {
+import java.util.List;
+
+public class NutritionCreatePlanFragment extends Fragment implements FirebaseAdmin.FirebaseAdminFavoriteSportAndNutrition {
 
 
     public NutritionCreatePlanFragment() {
         // Required empty public constructor
     }
 
+    private NutritionCreatePlanFragment fragment;
+
+    public NutritionCreatePlanFragment newInstanceSlimming() {
+        this.fragment = this;
+        return this.fragment;
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        SessionUser.getInstance().firebaseAdmin.setFirebaseAdminFavoriteSportAndNutrition(this);
+        SessionUser.getInstance().firebaseAdmin.downloadAllNutritionFavorite();
+    }
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private LinearLayout linearLayout;
+    private CreateNutritionPlanDetailsDialogFragment newFragment;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nutrition_create_plan, container, false);
+        View  view =  inflater.inflate(R.layout.fragment_nutrition_create_plan, container, false);
+
+        showLoading();
+        mRecyclerView = view.findViewById(R.id.recycler_view_nutrition);
+        linearLayout = view.findViewById(R.id.linear_empty_favorites);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new GridLayoutManager(getContext(), 2);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        return view;
     }
 
+    private ProgressDialog progressDialog;
+
+    public void showLoading() {
+        if (progressDialog != null && progressDialog.isShowing()) {
+            return;
+        }
+        progressDialog = new ProgressDialog(getContext(), R.style.TransparentProgressDialog);
+        progressDialog.show();
+        progressDialog.setContentView(R.layout.progress_dialog);
+        progressDialog.setCancelable(false);
+        RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotate.setInterpolator(new LinearInterpolator());
+        rotate.setDuration(1000);
+        rotate.setRepeatCount(Animation.INFINITE);
+        ImageView ivLoading = ButterKnife.findById(progressDialog, R.id.image_cards_animation);
+        ivLoading.startAnimation(rotate);
+        progressDialog.show();
+    }
+
+    public void hideLoading() {
+        if (progressDialog != null) {
+            progressDialog.dismiss();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        hideLoading();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        hideLoading();
+    }
+
+    @Override
+    public void downloandCollectionNutritionFavorite(boolean end) {
+        if(end==true){
+            hideLoading();
+            linearLayout.setVisibility(View.GONE);
+            mRecyclerView.setVisibility(View.VISIBLE);
+            List<DefaultNutrition> allFavoriteFavorite = SessionUser.getInstance().firebaseAdmin.allNutritionFavorite;
+            mAdapter = new CreateNutritionPlanAdapter(allFavoriteFavorite, new CreateNutritionPlanAdapter.OnItemClickListener() {
+                @Override
+                public void onItemClick(DefaultNutrition item) {
+                    FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                    Fragment prev = getFragmentManager().findFragmentByTag("dialog");
+                    if (prev != null) {
+                        transaction.remove(prev);
+                    }
+                    transaction.addToBackStack(null);
+                    newFragment = new CreateNutritionPlanDetailsDialogFragment();
+                    //newFragment.setListener(fragment);
+                    newFragment.show(transaction, "dialog");
+                }
+            });
+            mRecyclerView.setAdapter(mAdapter);
+        }
+    }
+
+    @Override
+    public void emptyCollectionNutritionFavorite(boolean end) {
+        if(end==true){
+            hideLoading();
+            linearLayout.setVisibility(View.VISIBLE);
+            mRecyclerView.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void inserSportFavoriteFirebase(boolean end) {
+
+    }
+
+    @Override
+    public void inserNutritionFavoriteFirebase(boolean end) {
+
+    }
+
+    @Override
+    public void downloandCollectionSportFavorite(boolean end) {
+
+    }
+
+    @Override
+    public void emptyCollectionSportFavorite(boolean end) {
+
+    }
+
+    @Override
+    public void deleteFavoriteSport(boolean end) {
+
+    }
+
+    @Override
+    public void deleteFavoriteNutrition(boolean end) {
+
+    }
 }
